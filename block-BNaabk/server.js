b6f8bb265314ac06d9a82ac618d21f4d6afffbe1
Rahul kapoor <kapoorrahul304@gmail.com>
2023-05-05 09:59:09 +0530

    few changes

--- block-BNaabk/server.js
+++ block-BNaabk/server.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 var app = express();
 
-app.use((req,res,next)=>{
-    console.log(req.method, req.url);
-    next()
-})
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
-app.get('/',(req,res)=>{
-    res.send('Welcome');
-})
+app.get('/', (req, res) => {
+  res.send('Welcome to express');
+});
 
-app.listen(3000,()=>{
-    console.log('port listen on 3k');
-})
+app.listen(3000, () => {
+  console.log('port listen on 3k');
+});
